e999be15fb73465eab77fbdda71b4693fe8cd1c3
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-05-03 18:57:54 -0500

    fix(datepicker, hourpicker and space-assign-patient)
    describe logica to this components

--- trabajofinal-vue-avancep/src/app/communications/components/datepicker-profile-colleague.vue
+++ trabajofinal-vue-avancep/src/app/communications/components/datepicker-profile-colleague.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { ref } from "vue";
 
+
+import Calendar from 'primevue/calendar';
+
+
+const date = ref();
 </script>
 
 <template>
-
+  <div class="card flex justify-content-center">
+    <Calendar v-model="date" />
+  </div>
 </template>
 
 <style scoped>
