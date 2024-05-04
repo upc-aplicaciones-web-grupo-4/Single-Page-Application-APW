5e0990a92df7bc28d71fe45b2aa75f1368e951a1
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-03 21:38:41 -0500

    feat(diagnosis-and-treatment): built component

--- trabajofinal-vue-avancep/src/app/medical-record/components/diagnosis-and-treatment.vue
+++ trabajofinal-vue-avancep/src/app/medical-record/components/diagnosis-and-treatment.vue
@@ -1,11 +1,124 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="empty-column-container">
+    <div class="hola">
+      <div class="hola2">
+        Diagnostics <Button label="Add" @click="navigateTo('treatmentPatient')"></Button>
+      </div>
+      <div class="column-row-item">
+        <ul>
+          <br>
+          <br>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+    <div class="hola">
+      <div class="hola2">
+        Treatment and medication <Button label="Add" @click="navigateTo('treatmentPatient')"></Button>
+      </div>
+      <div class="column-row-item2">
+        <ul>
+          <br>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import { ref } from 'vue'
+import { Button } from 'primevue/button';
 
+export default {
+  components: {
+    Button
+  },
+  setup() {
+    const navigateTo = (route) => {
+      // Aquí puedes implementar la lógica de navegación
+    }
+
+    return {
+      navigateTo
+    }
+  }
+}
+</script>
+
+<style scoped>
+.empty-column-container {
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 10px;
+}
+.hola2 button {
+  background: #dfcae1;
+  float: right;
+  border-radius: 15px;
+}
+.hola2 {
+  color: black;
+  align-content: center;
+}
+
+.hola {
+  margin-right: 25px;
+  margin-left: 20px;
+}
+
+.column-row-item {
+  flex: 1 1;
+  clear:both;
+  background-color: #C0A0C3;
+  border-radius: 10px;
+  height: 500px;
+  width: 470px;
+  margin: 1%;
+}
+.column-row-item ul {
+  top: 0;
+  left: 0;
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.column-row-item ul li {
+  background: white;
+  width: 60%;
+  border-radius: 10px;
+  margin-bottom: 50px;
+  margin-left: 25px;
+  padding: 70px;
+}
+
+.column-row-item2 {
+  flex: 1 1;
+  clear:both;
+  background-color: #C0A0C3;
+  border-radius: 10px;
+  height: 500px;
+  width: 470px;
+  margin: 1%;
+}
+.column-row-item2 ul {
+  top: 0;
+  left: 0;
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.column-row-item2 ul li {
+  background: white;
+  width: 70%;
+  border-radius: 10px;
+  margin-bottom: 50px;
+  margin-left: 25px;
+  padding: 50px;
+}
 </style>
