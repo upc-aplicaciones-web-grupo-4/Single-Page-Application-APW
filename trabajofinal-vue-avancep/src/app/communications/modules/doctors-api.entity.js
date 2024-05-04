653f3eb5a53054a762e48a568919882f5ef3fb93
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-05-03 21:19:43 -0500

    fix(doctors-api.entity.js, doctor-view-colleague-doctors-api.service)
    Fake API functionality on doctor-view-colleague

---
+++ trabajofinal-vue-avancep/src/app/communications/modules/doctors-api.entity.js
@@ -0,0 +1,37 @@
+export class Doctor {
+    constructor(
+        id = '',
+        name = '',
+        lastname = '',
+        fee = 0,
+        gender = '',
+        image = '',
+        title = '',
+        college_number = '',
+        RNE = '',
+        undergraduate = '',
+        specialty = '',
+        sub_speciality = '',
+        code_of_doctor = '',
+        subscription_id = 0,
+        email = '',
+        password = ''
+    ){
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.fee = fee;
+        this.gender = gender;
+        this.image = image;
+        this.title = title;
+        this.college_number = college_number;
+        this.RNE = RNE;
+        this.undergraduate = undergraduate;
+        this.specialty = specialty;
+        this.sub_speciality = sub_speciality;
+        this.code_of_doctor = code_of_doctor;
+        this.subscription_id = subscription_id;
+        this.email = email;
+        this.password = password;
+    }
+}
--- trabajofinal-vue-avancep/src/app/communications/pages/doctor-view-colleague.vue
+++ trabajofinal-vue-avancep/src/app/communications/pages/doctor-view-colleague.vue
@@ -1,11 +1,71 @@
 <script setup>
+import { ref } from 'vue';
+import { DoctorsApiService } from "../services/doctors-api.service.js";
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 
+const apiService = new DoctorsApiService();
+const searchEmail = ref('');
+const doctorData = ref(null);
+const visible = ref(false);
+
+const searchDoctor = async () => {
+  try {
+    const doctor = await apiService.getDoctorByEmail(searchEmail.value);
+    if (doctor) {
+      doctorData.value = doctor;
+      visible.value = true; // Mostrar el diálogo
+    } else {
+      // Mostrar mensaje de error si el doctor no se encuentra
+      console.error('Doctor not found');
+    }
+  } catch (error) {
+    console.error('Error searching doctor:', error);
+  }
+};
+
+const closeModal = () => {
+  visible.value = false; // Cerrar el diálogo
+};
 </script>
-
 <template>
+  <div>
+    <input v-model="searchEmail" @keyup.enter="searchDoctor" placeholder="Enter doctor's email">
+    <Button label="Show" @click="searchDoctor" />
 
+    <Dialog v-model:visible="visible" modal header="Doctor Information" :style="{ width: '90rem' }">
+
+      <div class="group-total" v-if="doctorData">
+        <div class="column-left">
+          <div class="cont">
+            <div class="card flex justify-content-center">
+              <img :src="doctorData.image" alt="Profile Image" width="250" class="rounded-image"/>
+            </div>
+          </div>
+        </div>
+
+        <div class="column-middle">
+          <div class="information">
+            <div>
+              <!-- Use doctorData to display doctor's information -->
+              <p>Name: {{ doctorData.name }}</p>
+              <p>Lastname: {{ doctorData.lastname }}</p>
+              <p>Email: {{ doctorData.email }}</p>
+              <p>School N: {{ doctorData.college_number }}</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column-right">
+          <div>
+            <h2>Titles: </h2>
+            <div class="card flex justify-content-center">
+              <img :src="doctorData.title" alt="Title Image" width="250" class="rounded-image"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </Dialog>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
