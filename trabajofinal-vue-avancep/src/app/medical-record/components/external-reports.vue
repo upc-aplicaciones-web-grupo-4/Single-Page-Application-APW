abcc65eafe42e74b16355695549bf1be55df6c26
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-03 21:39:10 -0500

    feat(external-reports): built component

--- trabajofinal-vue-avancep/src/app/medical-record/components/external-reports.vue
+++ trabajofinal-vue-avancep/src/app/medical-record/components/external-reports.vue
@@ -1,11 +1,30 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="empty-column-container">
+    <div class="empty-column-item">imagen aqui</div>
+    <div class="empty-column-item">imagen aqui</div>
+    <div class="empty-column-item">imagen aqui</div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  setup() {
+    // Aquí puedes agregar la lógica del componente si es necesario
+  }
+}
+</script>
 
+<style scoped>
+.empty-column-container {
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 10px;
+}
+
+.empty-column-item {
+  flex: 1 1 30%;
+  background-color: #f2f2f2;
+  height: 500px;
+  margin: 1%;
+}
 </style>
