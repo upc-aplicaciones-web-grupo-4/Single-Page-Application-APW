3a8c52d6f2de8205b46f417a36a411d0a5bf0f85
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-03 21:39:19 -0500

    feat(header): built component

--- trabajofinal-vue-avancep/src/app/medical-record/components/header.vue
+++ trabajofinal-vue-avancep/src/app/medical-record/components/header.vue
@@ -1,11 +1,34 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="content">
+    <Toolbar color="primary">
+      <Button icon="pi pi-arrow-left"></Button>
+      <span>Clinical history of patient Carlos Perez</span>
+    </Toolbar>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import { Button } from 'primevue/button';
+import { Toolbar } from 'primevue/toolbar';
 
+export default {
+  components: {
+    Button,
+    Toolbar
+  },
+  setup() {
+    // Aquí puedes agregar la lógica del componente si es necesario
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  background: #A788AB;
+}
+
+.p-toolbar {
+  padding: 10px;
+  background-color: #A788AB;
+}
 </style>
