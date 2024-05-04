45fcc11572b53f488c833fa1f824540d5a459456
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-03 21:38:24 -0500

    feat(clinical-history): built component

--- trabajofinal-vue-avancep/src/app/medical-record/components/clinical-history.vue
+++ trabajofinal-vue-avancep/src/app/medical-record/components/clinical-history.vue
@@ -1,11 +1,60 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="div-toolbar">
+    <Toolbar class="buttons" color="primary">
+      <Button label="Reason of consultation" @click="navigateTo('reasonConsultation')"></Button>
+      <Button label="Background" @click="navigateTo('background')"></Button>
+      <Button label="Medical exams" @click="navigateTo('medicalExams')"></Button>
+      <Button label="External reports" @click="navigateTo('externalReports')"></Button>
+      <Button label="Diagnoses and treatment" @click="navigateTo('diagnosesAndTreatment')"></Button>
+    </Toolbar>
+  </div>
 </template>
 
+<script>
+import { ref } from 'vue'
+import { Button } from 'primevue/button';
+import { Toolbar } from 'primevue/toolbar';
+
+export default {
+  components: {
+    Button,
+    Toolbar
+  },
+  setup() {
+    const navigateTo = (route) => {
+      // Aquí puedes implementar la lógica de navegación
+    }
+
+    return {
+      navigateTo
+    }
+  }
+}
+</script>
+
 <style scoped>
+button {
+  background-color: #dfcae1;
+  color: black !important;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 0 10px;
+}
+
+.div-toolbar {
+  background: #A788AB;
+  border-radius: 10px;
+}
+
+.buttons {
+  justify-content: center;
+}
+
+.toolbar {
+  display: flex;
+  justify-content: center;
+}
 
 </style>
---
+++ trabajofinal-vue-avancep/src/app/medical-record/pages/medical-record.pages.vue
@@ -0,0 +1,51 @@
+<template>
+  <header></header>
+  <div class="container">
+    <div class="patient-data">
+      <patient-data></patient-data>
+    </div>
+    <div class="other-component">
+      <clinicalHistory></clinicalHistory>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import header from "@/app/medical-record/components/header.vue"
+import patientData from "@/app/medical-record/components/patient-data.vue"
+import clinicalHistory from "@/app/medical-record/components/clinical-history.vue"
+
+export default {
+  components: {
+    header,
+    patientData,
+    clinicalHistory
+  },
+}
+</script>
+
+<style>
+@import 'primevue/resources/themes/saga-blue/theme.css';
+@import 'primevue/resources/primevue.min.css';
+
+.container {
+  display: flex;
+  margin-top: 20px; /* Añade un margen superior para separarlo del encabezado */
+}
+
+.patient-data {
+  /* Estilos para el componente de datos del paciente */
+  flex: 1;
+  margin-left: 20px; /* Espacio entre el componente y el otro componente */
+  margin-right: -50%;
+}
+
+.other-component {
+  /* Estilos para el otro componente */
+  margin-right: 30px;
+  background: #A788AB;
+  border-radius: 8px;
+  flex: 1;
+}
+</style>
