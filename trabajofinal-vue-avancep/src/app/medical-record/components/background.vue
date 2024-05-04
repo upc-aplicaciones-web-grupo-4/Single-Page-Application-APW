98ed7209edcf3b02c35ace500dd09c5d993159f1
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-03 21:38:11 -0500

    feat(background): built component

--- trabajofinal-vue-avancep/src/app/medical-record/components/background.vue
+++ trabajofinal-vue-avancep/src/app/medical-record/components/background.vue
@@ -1,11 +1,53 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="two-column-container">
+    <div class="two-column-title">Personal history</div>
+    <InputTextarea class="two-column-item" v-model="personalHistory"></InputTextarea>
+    <div class="two-column-title">Family background</div>
+    <InputTextarea class="two-column-item" v-model="familyBackground"></InputTextarea>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import { ref } from 'vue'
+import { InputTextarea } from 'primevue/inputtextarea';
 
+export default {
+  components: {
+    InputTextarea
+  },
+  setup() {
+    const personalHistory = ref('');
+    const familyBackground = ref('');
+
+    return {
+      personalHistory,
+      familyBackground
+    }
+  }
+}
+</script>
+
+<style scoped>
+.two-column-container {
+  display: flex;
+  height: 80%;
+  width: 80%;
+  margin-left: 40px;
+}
+
+.two-column-item {
+  flex: 1;
+  padding: 00px;
+  box-sizing: border-box;
+  margin-top: 40px;
+  margin-left: -150px;
+}
+
+.two-column-title {
+  font-size: 1.2em;
+  font-weight: bold;
+  padding: 10px;
+  color: #000000;
+  text-align: center;
+}
 </style>
