d0c0fa2b6a1a205c4346478d938f221ac0dcb111
hernancit0 <ernanemiliom12@gmail.com>
2024-06-05 18:01:37 -0500

    fix: update component
    update doctor-patient page

--- src/app/communications/pages/doctor-patient.pages.vue
+++ src/app/communications/pages/doctor-patient.pages.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="container">
-    <ChatListDoctor class="chat-list" />
     <DoctorChatPages class="chat-container" />
   </div>
 </template>
 
 <script>
 // Importa los componentes necesarios
-import ChatListDoctor from '@/app/communications/components/chat-list-doctor.vue';
 import DoctorChatPages from "@/app/communications/components/doctor-chat.pages.vue";
 
 export default {
   components: {
-    ChatListDoctor,
     DoctorChatPages
   }
 }
 </script>
 
 <style scoped>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 .container {
   display: flex;
+  margin: 0;
+  padding: 0;
 
 }
 </style>
